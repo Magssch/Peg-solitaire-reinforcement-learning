83fd7f4f4453bdeffe07637d4b4322c864899cc8
Nicklas Bekkevold <nicklasbekkevold@gmail.com>
2021-01-11 18:44:19 +0100

    Implement board shapes

--- src/simulated_world.py
+++ src/simulated_world.py
@@ -7,10 +7,23 @@
 # - Generates child states from parent states using the legal operators of the
 #  domain.
 # - Recognizes final (winning, losing and neutral) states
+from enum import Enum
+import numpy as np
+
+
+class Shape(Enum):
+    Diamond = 1
+    Triangle = 2
+
+
 class SimulatedWorld:
 
-    def __init__(self):
-        pass
+    def __init__(self, shape, size):
+        self.shape = shape
+        self.size = size
+        self.board = np.zeros((size, size))
+        if shape == Shape.Triangle:
+            self.board = np.triu(np.full((size, size), None), 1)
 
     def produce_initial_state(self):
         pass
@@ -19,4 +32,11 @@ class SimulatedWorld:
         pass
 
     def is_final_state(self):
-        pass
+        return (self.board == 1).sum() == 1
+
+
+if __name__ == "__main__":
+    world = SimulatedWorld(Shape.Triangle, 4)
+    world.board[1, 0] = 1
+    print(world.board)
+    print(world.is_final_state())
