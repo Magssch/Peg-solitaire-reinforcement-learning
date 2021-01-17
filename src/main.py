025da1670ee391bb08fee0e5d6758d4e02a10428
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2021-01-17 23:36:13 +0100

    Refactor pivotal parameters and add boilerplate code to simulated env

--- src/main.py
+++ src/main.py
@@ -1,28 +1,19 @@
 from environment import Environment
-from reinforcement_learner import ReinforcementLearner
+import json
+#from reinforcement_learner import ReinforcementLearner
 from simulated_world import Shape, SimulatedWorld
 
 if __name__ == "__main__":
-    # Pivotal paramters / env
     env = Environment()
-    env.board_type = Shape.Diamond
-    env.size = 4
-    env.holes = []
+    with open('src/pivotal_parameters.json', 'r') as f:
+        pivotal_parameters = json.load(f)
+        for attr, value in pivotal_parameters.items():
+            setattr(env, attr, value)
+        if env.board_type == 1:
+            env.board_type = Shape.Diamond
+        else:
+            env.board_type = Shape.Triangle
+    print(env.board_type)
 
-    env.episodes = 500
-    env.ANN_dimentions = (15, 20, 30, 5, 1)
-
-    env.actor_learning_rate = 0.4
-    env.actor_trace_decay = 0.9
-    env.actor_discount_factor = 0.9
-    env.actor_epsilon = 0.5
-    env.actor_epsilon_decay_rate = 0.5
-
-    env.critic_learning_rate = 0.1
-    env.critic_trace_decay = 0.9
-    env.critic_discount_factor = 0.9
-
-    env.visualize_games = False
-
-    world = SimulatedWorld(env.board_type, env.size)
-    agent = ReinforcementLearner(env)
+   world = SimulatedWorld(env.board_type, env.size)
+   agent = ReinforcementLearner(env)
