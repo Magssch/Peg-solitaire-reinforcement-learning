1837754a0db74efc3cf1027c75eb8aec5daa3a28
Nicklas Bekkevold <nicklasbekkevold@gmail.com>
2021-01-19 11:26:11 +0100

    Refactor critic

--- src/critic.py
+++ src/critic.py
@@ -5,28 +5,117 @@ from keras import backend as K
 from keras.layers import Dense, Input
 from keras.models import Sequential
 from keras.optimizers import Adam
+from environment import Environment
+import random
+
+from abc import ABC, abstractmethod
 
 
-class Critic:
+class CriticFactory:
+
+    @classmethod
+    def get_critic(cls, env: Environment):
+        if env.critic_nn_dimensions is None:
+            return TableCritic(
+                env.critic_learning_rate,
+                env.critic_trace_decay,
+                env.critic_discount_factor,
+                None,
+            )
+        else:
+            return NNCritic(
+                env.critic_learning_rate,
+                env.critic_trace_decay,
+                env.critic_discount_factor,
+                env.critic_nn_dimensions,
+            )
+
+
+class Critic(ABC):
 
     def __init__(
         self,
         learning_rate: float,
         trace_decay: float,
         discount_factor: float,
-        nn_dimensions: tuple = None,
     ):
         self.__learning_rate = learning_rate  # alpha
         self.__trace_decay = trace_decay  # lambda
         self.__discount_factor = discount_factor  # gamma
 
+    def td_error(self, current_state, successor_state, reward) -> float:
+        return reward + self.__discount_factor * self.get_value(successor_state) - self.get_value(current_state)
+
+    @abstractmethod
+    def get_value(self, state) -> float:
+        pass
+
+    @abstractmethod
+    def update_value(self, state, td_error) -> None:
+        pass
+
+    @abstractmethod
+    def reset_eligibilities(self) -> None:
+        pass
+
+    @abstractmethod
+    def replace_eligibilities(self, state) -> None:
+        pass
+
+    @abstractmethod
+    def update_eligibilities(self, state) -> None:
+        pass
+
+
+class TableCritic(Critic):
+
+    def __init__(
+        self,
+        learning_rate: float,
+        trace_decay: float,
+        discount_factor: float,
+        nn_dimensions: tuple = None
+    ):
+        super().__init__(
+            learning_rate,
+            trace_decay,
+            discount_factor,
+        )
+        self.__values = defaultdict(lambda: random.random() * 0.2)
+
+    def get_value(self, state) -> float:
+        return self.__values[state]
+
+    def update_value(self, state, td_error) -> None:
+        self.__values[state] += self.__learning_rate * td_error * self.eligibilities[state]
+
+    def reset_eligibilities(self) -> None:
+        self.eligibilities = defaultdict(float)
+
+    def replace_eligibilities(self, state) -> None:
+        self.eligibilities[state] = 1
+
+    def update_eligibilities(self, state) -> None:
+        self.eligibilities[state] *= self.__discount_factor * self.__trace_decay
+
+
+class NNCritic(Critic):
+
+    def __init__(
+        self,
+        learning_rate: float,
+        trace_decay: float,
+        discount_factor: float,
+        nn_dimensions: tuple
+    ):
+        super().__init__(
+            learning_rate,
+            trace_decay,
+            discount_factor,
+        )
         self.__nn_dimensions = nn_dimensions
-
-        self.__values = {}  # V
-        self.reset_eligibilities()
-
-        if nn_dimensions is not None:
-            self.model = self.__build_critic_network(nn_dimensions)
+        assert nn_dimensions is not None
+        self.values = self.__build_critic_network(nn_dimensions)
 
     def __build_critic_network(self, nn_dimensions: tuple) -> Sequential:
         input_dim, *hidden_dims, output_dim = nn_dimensions
@@ -48,33 +137,29 @@ class Critic:
         model.summary()
         return model
 
-    def get_values(self, state) -> float:
-        if self.__nn_dimensions is not None:
-            return np.squeeze(self.model(state))
-        return self.__values[state]
+    def get_value(self, state) -> float:
+        return np.squeeze(self.values(state))
 
     def td_error(self, current_state, successor_state, reward) -> float:
-        return reward + self.__discount_factor * self.get_values(successor_state) - self.get_values(current_state)
+        return reward + self.__discount_factor * self.get_value(successor_state) - self.get_value(current_state)
 
     def update_value(self, state, td_error) -> None:
-        self.__values[state] += self.__learning_rate * td_error * self.eligibilities[state]
+        pass
 
     def reset_eligibilities(self) -> None:
-        if self.__nn_dimensions is not None:
-            input_dim, *hidden_dims, _ = self.__nn_dimensions
-            self.eligibilities = [np.zeros(input_dim)]
-            for dimension in hidden_dims:
-                self.eligibilities.append(np.zeros(dimension))
-        else:
-            self.eligibilities = defaultdict(float)
+        input_dim, *hidden_dims, _ = self.__nn_dimensions
+        self.eligibilities = [np.zeros(input_dim)]
+        for dimension in hidden_dims:
+            self.eligibilities.append(np.zeros(dimension))
 
-    def replace_trace(self, state) -> None:
-        self.eligibilities[state] = 1
+    def replace_eligibilities(self, state) -> None:
+        pass
 
-    def update_trace(self, state) -> None:
-        self.eligibilities[state] *= self.__discount_factor * self.__trace_decay  # + nabla V(s) for NNs
+    def update_eligibilities(self, state) -> None:
+        pass
 
 
 if __name__ == "__main__":
-    critic = Critic(0.01, 0.9, 0.01)
+    env = Environment()
+    critic = CriticFactory.get_critic(env)
     critic.reset_eligibilities()
--- src/reinforcement_learner.py
+++ src/reinforcement_learner.py
@@ -1,5 +1,5 @@
 from actor import Actor
-from critic import Critic
+from critic import Critic, CriticFactory
 from main import Environment
 from simulated_world import Shape, SimulatedWorld
 
@@ -14,12 +14,8 @@ class ReinforcementLearner:
             env.actor_epsilon,
             env.actor_nn_dimensions,
         )
-        self.critic = Critic(
-            env.critic_learning_rate,
-            env.critic_trace_decay,
-            env.critic_discount_factor,
-            env.critic_nn_dimensions,
-        )
+        self.critic = CriticFactory.get_critic(env)
+
         self.simulated_world = SimulatedWorld(Shape.Diamond, 4)
         self.episodes = env.episodes
 
@@ -31,23 +27,25 @@ class ReinforcementLearner:
             self.critic.reset_eligibilities()
 
             state = self.simulated_world.produce_initial_state()
-            action = self.actor.choose_mixed(state)
+            action = self.actor.choose_action(state)
 
-            while not self.simulated_world.is_final_state():
+            done = False
 
-                next_state, reward = self.simulated_world.do_action(action)
-                next_action = self.actor.choose_mixed(next_state)
+            while not done:
 
-                self.actor.replace_trace(state, action)
-                self.critic.replace_trace(state)
+                next_state, reward, done = self.simulated_world.step(action)
+                next_action = self.actor.choose_action(next_state)
+
+                self.actor.replace_eligibilities(state, action)
+                self.critic.replace_eligibilities(state)
 
                 td_error = self.critic.td_error(state, next_state, reward)
 
                 # for (s, a) in episode:
                 self.actor.update_policy(state, action, td_error)
-                self.actor.update_trace(state, action)
+                self.actor.update_eligibilities(state, action)
                 self.critic.update_value(state, td_error)
-                self.critic.update_trace(state)
+                self.critic.update_eligibilities(state)
 
                 state, action = next_state, next_action
 
