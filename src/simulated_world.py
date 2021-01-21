27803ae91ccd94013a25bdf8a1dfa3b3e80ac534
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2021-01-21 19:20:50 +0100

    WIP: Improve simulated world

--- src/simulated_world.py
+++ src/simulated_world.py
@@ -17,6 +17,10 @@ class Shape(Enum):
     Diamond = 1
     Triangle = 2
 
+# move = (coordinates for)
+# action = (starting_index_position, ending_index_position)
+# move = ()
+
 
 class SimulatedWorld:
 
@@ -24,13 +28,13 @@ class SimulatedWorld:
         self.__board_type = board_type
         self.__size = size
         self.__board = np.zeros(size**2)
-        self.__adjacent_cells = (
+        self.__edges = (
             (0, -1),
             (1, -1),
             (1, 0),
             (0, 1),
             (-1, 1),
-            (0, -1),
+            (-1, 0),
         )
         if self.__board_type == Shape.Triangle:
             self.__board = np.triu(np.full((size, size), None), 1)
@@ -39,9 +43,14 @@ class SimulatedWorld:
         pass
         # reset to
 
-    def step(self, state) -> tuple:
-        pass
-        # Go to next state
+    def step(self, action: tuple(int, int)) -> tuple(int):
+        start_index, end_index = action
+        start_coordinates = self.__coordinates_from_index(start_index)
+        end_coordinates = self.__coordinates_from_index(end_index)
+        if self.__is_legal_move(start_coordinates, end_coordinates):
+            self.__board[start_index] = 0
+            self.__board[start_index] = 0
+            self.__board[end_index] = 1
 
     def draw_board(self) -> None:
         pass
@@ -51,37 +60,44 @@ class SimulatedWorld:
 
     def __is_legal_move(self, coordinates: tuple(int, int), move: tuple(int, int)) -> bool:
         return self.__move_is_inside_board(coordinates, move) and \
-                self.__cell_contains_peg((coordinates[0]+move[0], coordinates[1]+move[1])) and \
-                self.__move_is_inside_board(coordinates, (move[0]*2, move[1]*2)) and \
-                not self.__cell_contains_peg((coordinates[0]+(move[0]*2), coordinates[1]+(move[1]*2)))
+            self.__cell_contains_peg((coordinates[0] + move[0], coordinates[1] + move[1])) and \
+            self.__move_is_inside_board(coordinates, (move[0] * 2, move[1] * 2)) and \
+            not self.__cell_contains_peg(
+                (coordinates[0] + (move[0] * 2), coordinates[1] + (move[1] * 2)))
 
     def __cell_contains_peg(self, cell_position: tuple(int, int)) -> bool:
-        return self.__board[self.__get_index_by_coordinates(cell_position)] == 1
+        return self.__board[self.__index_from_coordinates(cell_position)] == 1
 
     def __move_is_inside_board(self, cell_position: tuple(int, int), move: tuple(int, int)) -> bool:
         return (cell_position[0] + move[0] > 0 and cell_position[0] + move[0] < self.__size) \
-                and (cell_position[1] + move[1] > 0 and cell_position[1] + move[1] < self.__size)
+            and (cell_position[1] + move[1] > 0 and cell_position[1] + move[1] < self.__size)
 
-    def __get_coordinates_for_position(self, position: int) -> tuple(int, int):
+    def __coordinates_from_index(self, position: int) -> tuple(int, int):
         """
         Converts numerical 1D-index to a tuple of 2D-coordinates
         """
         return position // self.__size, position % self.__size
 
-    def __get_index_by_coordinates(self, cell_position: tuple(int, int)) -> int:
+    def __index_from_coordinates(self, cell_position: tuple(int, int)) -> int:
         """
         Converts 2D-coordinates to an 1D-index position
         """
-        row = cell_position[0]*self.__size
+        row = cell_position[0] * self.__size
         return row, row + cell_position[1]
+
+    def __direction_index_from_vector(self, direction_vector: tuple(int, int)) -> int:
+        """
+        Converts 2D-direction vector to an index
+        """
+        return self.__edges.index(direction_vector)
 
     def __get_legal_moves_for_position(self, position: int) -> tuple(int):
         legal_moves = []
-        coordinates = self.__get_coordinates_for_position(position)
+        coordinates = self.__coordinates_from_index(position)
 
-        for move in self.__adjacent_cells:
+        for move in self.__edges:
             if self.__is_legal_move(coordinates, move):
-                legal_moves.append(self.__get_index_by_coordinates((coordinates[0]+(move[0]*2), coordinates[1]+(move[1]*2))))
+                legal_moves.append(self.__index_from_coordinates(move))
 
         return tuple(legal_moves)
 
@@ -89,5 +105,6 @@ class SimulatedWorld:
         legal_moves = []
         i = 0
         for position in self.__board:
-            legal_moves[i] = (position, self.__get_legal_moves_for_position(position))
+            legal_moves[i] = (
+                position, self.__get_legal_moves_for_position(position))
         return tuple(legal_moves)
