bc12e662ae7216a92197de6e56ee184d5c3ec944
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2021-01-21 11:23:11 +0100

    Add logic for game board

--- src/hexagonal_board.py
+++ src/hexagonal_board.py
@@ -11,3 +11,17 @@ class Diamond(HexagonalBoard):
 class Triangle(HexagonalBoard):
     def __init__(self):
         pass
+
+
+class Action():
+    from_position: int
+    to_position: int
+
+class Cell():
+
+    def __init__(self) -> None:
+        pass
+    
+    def __str__(self) -> str:
+        pass
+
--- src/simulated_world.py
+++ src/simulated_world.py
@@ -23,7 +23,7 @@ class SimulatedWorld:
     def __init__(self, board_type: Shape, size: int):
         self.__board_type = board_type
         self.__size = size
-        self.__board = np.zeros((size, size))
+        self.__board = np.zeros(size**2)
         self.__adjacent_cells = (
             (0, -1),
             (1, -1),
@@ -35,21 +35,54 @@ class SimulatedWorld:
         if self.__board_type == Shape.Triangle:
             self.__board = np.triu(np.full((size, size), None), 1)
 
-    def produce_initial_state(self) -> None:
+    def reset(self) -> None:
         pass
+        # reset to 
 
-    def generate_child_states(self) -> None:
+    def step(self) -> tuple:
         pass
+        # Go to next state
 
+    def draw_board(self) -> None:
+        # return (self.__board, #poeng, self.is_final_state())
+        pass
+    
     def is_final_state(self) -> bool:
         pass
         # return (self.__board == 1).sum() == 1
-
-    def draw_board(self) -> None:
-        pass
 
     def __is_legal_move(self) -> bool:
         pass
 
     def __is_adjacent(self, x: int, y: int) -> bool:
         return (x, y) in self.__adjacent_cells
+
+    def __move_is_inside_board(self, cell_position: int, move: int) -> bool:
+        return cell_position + move > 0 and cell_position + move < self.__size
+
+    def __get_coordinates_for_position(self, position: int) -> tuple(int, int):
+        """
+        Converts numerical 1D-index to a tuple of 2D-coordinates
+        """
+        return position // self.__size, position % self.__size
+
+    def __get_legal_actions_for_position(self, position: int) -> tuple(int, int):
+        legal_moves = []
+        row, col = self.__get_coordinates_for_position(position)
+
+        for adjacent_row, adjacent_col in self.__adjacent_cells:
+            if self.__move_is_inside_board(row, adjacent_row) and self.__move_is_inside_board(col, adjacent_col):
+                # TODO: CHECK IF THERE IS A PEG TO JUMP OVER
+                # Generate cell position tuples for cells beyond pegs
+                landing_cells = [tuple((a*2, b*2) for a, b in adjacent_cell) for adjacent_cell in self.__adjacent_cells]
+                for landing_row, landing_col in landing_cells:
+                    if self.__move_is_inside_board(row, adjacent_row) and self.__move_is_inside_board(col, adjacent_col):
+                        legal_moves.append((landing_row, landing_col))
+
+        return tuple(legal_moves)
+
+                    
+
+    def get_all_legal_actions(self) -> tuple(tuple):
+        for cell in self.__board:
+            pass
