5de4067bbf54eb828d4d52977954fb827dc44f8d
Nicklas Bekkevold <nicklasbekkevold@gmail.com>
2021-01-26 20:08:38 +0100

    Add docstring to CriticFactory

--- src/critic/critic_factory.py
+++ src/critic/critic_factory.py
@@ -5,15 +5,34 @@ from .table_critic import TableCritic
 
 class CriticFactory:
 
-    @classmethod
+    @staticmethod
     def get_critic(
-        cls,
         use_table_critic: bool,
         critic_learning_rate: float,
         critic_discount_factor: float,
         critic_trace_decay: float,
         critic_nn_dimensions: tuple,
     ) -> Critic:
+        """
+        Constructs either a TableCritic or NNCritic based on use_table_critic.
+
+        Parameters
+        ----------
+            use_table_critic : bool
+                Whether or not to use table Critic or NN-based Critic
+            critic_learning_rate : float
+                Learning rate for the Critic
+            critic_discount_factor : float
+                Discount factor for the Critic
+            critic_trace_decay : float
+                Trace decay for the Critic
+            critic_nn_dimensions : tuple
+                Dimensions for the neural network (if used)
+
+        Returns
+        -------
+        Critic
+        """
         if use_table_critic:
             return TableCritic(
                 critic_learning_rate,
