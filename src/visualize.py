d8645ae2cac9dd2a80c00bcf24eee29f3545e726
Lars-Magnus Underhaug <larsmagu@gmail.com>
2021-02-02 17:05:27 +0100

    Rename class

--- src/visualize.py
+++ src/visualize.py
@@ -4,7 +4,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 
-class DrawBoard():
+class Visualize():
 
     graph = nx.Graph()
 
@@ -47,45 +47,43 @@ class DrawBoard():
     def initialize_board(cls, board, size, edges, board_type):
         cls.graph = nx.Graph()
 
-        legal_positions = DrawBoard.get_legal_positions(board)
+        legal_positions = Visualize.get_legal_positions(board)
 
         # Creates a Hex Diamon grid
         if board_type == Shape.Diamond:
             for i in range(size):
                 for j in range(size):
-                    DrawBoard.add_node_to_graph(DrawBoard.graph, (i,j))
+                    Visualize.add_node_to_graph(Visualize.graph, (i,j))
 
         # Create a Hex Triangle grid
         elif board_type == Shape.Triangle:
             for i in range(size):
                 for j in range(i + 1):
-                    DrawBoard.add_node_to_graph(DrawBoard.graph, (i, j))
+                    Visualize.add_node_to_graph(Visualize.graph, (i, j))
 
         for x, y in legal_positions:
             for row_offset, column_offset in edges:
                 neighbor_node = (x + row_offset, y + column_offset)
                 if neighbor_node in legal_positions:
-                    DrawBoard.add_edge_to_graph(DrawBoard.graph, (x, y), neighbor_node)
-
+                    Visualize.add_edge_to_graph(Visualize.graph, (x, y), neighbor_node)
 
     @staticmethod
     def draw_board(board, action_nodes, positions=None):
 
         # List of all node positions currently filled
-        filled_nodes = DrawBoard.get_filled_nodes(board)
-        empty_nodes = DrawBoard.get_empty_nodes(board)
-
+        filled_nodes = Visualize.get_filled_nodes(board)
+        empty_nodes = Visualize.get_empty_nodes(board)
 
         # Remove nodes currently active
         for nodes in action_nodes:
             filled_nodes.remove(nodes)
 
         # Draw the resulting grid
-        nx.draw_networkx_nodes(DrawBoard.graph, positions, nodelist=empty_nodes, node_color='black')
-        nx.draw_networkx_nodes(DrawBoard.graph, positions, nodelist=filled_nodes, node_color='blue')
-        nx.draw_networkx_nodes(DrawBoard.graph, positions, nodelist=action_nodes[0], node_color='green')
-        nx.draw_networkx_nodes(DrawBoard.graph, positions, nodelist=action_nodes[1], node_color='red')
-        nx.draw_networkx_edges(DrawBoard.graph, positions, width=1)
+        nx.draw_networkx_nodes(Visualize.graph, positions, nodelist=empty_nodes, node_color='black')
+        nx.draw_networkx_nodes(Visualize.graph, positions, nodelist=filled_nodes, node_color='blue')
+        nx.draw_networkx_nodes(Visualize.graph, positions, nodelist=action_nodes[0], node_color='green')
+        nx.draw_networkx_nodes(Visualize.graph, positions, nodelist=action_nodes[1], node_color='red')
+        nx.draw_networkx_edges(Visualize.graph, positions, width=1)
 
         plt.axis('off')
         plt.draw()
