3cb29e1da4a7a2200b982a0ab2df84eaf76cdaeb
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2021-02-04 11:56:09 +0100

    Add tostring to game board

--- src/hexagonal_board.py
+++ src/hexagonal_board.py
@@ -82,6 +82,9 @@ class HexagonalBoard(ABC):
                     legal_moves += legal_actions_for_position
         return tuple(legal_moves)
 
+    def __str__(self):
+        return self.__board
+
 
 class Diamond(HexagonalBoard):
     def __init__(self, board_type: Shape, size: int, holes: Set[Tuple[int, int]]):
