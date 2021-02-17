6fb7412574ab8f17285aa681d96432c27045388f
Nicklas Bekkevold <nicklasbekkevold@gmail.com>
2021-02-17 17:46:46 +0100

    Rename paramter files

--- src/parameters/D4_nn_triangle_6.py
+++ src/parameters/D4_nn_triangle_6.py
@@ -1,20 +1,18 @@
 from data_classes import Shape
 
 # General
-EPISODES = 250
+EPISODES = 150
 VISUALIZE_GAMES = True
 FRAME_DELAY = 0.15
 
 # Simulated World
-BOARD_TYPE = Shape.Diamond
+BOARD_TYPE = Shape.Triangle
 SIZE = 6
 HOLES = set([
     # (1, 1), Not solvable
     (1, 1),  # C_a
     (5, 1),  # C_a
     (3, 2),  # C_a
-    (2, 3),  # C_a
-    (0, 0),  # C_a
     # (2, 1),  # C_b
     # (2, 2), Not solvable
 ])
@@ -32,6 +30,6 @@ CRITIC_LEARNING_RATE = 0.001
 CRITIC_DISCOUNT_FACTOR = 0.85
 CRITIC_TRACE_DECAY = 0.85
 
-USE_TABLE_CRITIC = True
-INPUT_DIMENSION = SIZE**2 if BOARD_TYPE == Shape.Diamond else int((SIZE * (SIZE + 1)) / 2)
+USE_TABLE_CRITIC = False
+INPUT_DIMENSION = SIZE ** 2 if BOARD_TYPE == Shape.Diamond else int((SIZE * (SIZE + 1)) / 2)
 CRITIC_NN_DIMENSIONS = (INPUT_DIMENSION, 20, 30, 5, 1)
--- src/parameters/D4_table_critic_diamond_6.py
+++ src/parameters/D4_table_critic_diamond_6.py
@@ -1,18 +1,20 @@
 from data_classes import Shape
 
 # General
-EPISODES = 150
+EPISODES = 250
 VISUALIZE_GAMES = True
 FRAME_DELAY = 0.15
 
 # Simulated World
-BOARD_TYPE = Shape.Triangle
+BOARD_TYPE = Shape.Diamond
 SIZE = 6
 HOLES = set([
     # (1, 1), Not solvable
     (1, 1),  # C_a
     (5, 1),  # C_a
     (3, 2),  # C_a
+    (2, 3),  # C_a
+    (0, 0),  # C_a
     # (2, 1),  # C_b
     # (2, 2), Not solvable
 ])
@@ -30,6 +32,6 @@ CRITIC_LEARNING_RATE = 0.001
 CRITIC_DISCOUNT_FACTOR = 0.85
 CRITIC_TRACE_DECAY = 0.85
 
-USE_TABLE_CRITIC = False
-INPUT_DIMENSION = SIZE**2 if BOARD_TYPE == Shape.Diamond else int((SIZE * (SIZE + 1)) / 2)
+USE_TABLE_CRITIC = True
+INPUT_DIMENSION = SIZE ** 2 if BOARD_TYPE == Shape.Diamond else int((SIZE * (SIZE + 1)) / 2)
 CRITIC_NN_DIMENSIONS = (INPUT_DIMENSION, 20, 30, 5, 1)
